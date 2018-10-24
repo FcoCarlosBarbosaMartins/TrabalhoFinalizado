f5cdc9488ce83a6842e3899dd6d128120a31984e
alunoti <alunoti@CEPGBESL05004.estacio.acad>
2018-10-24 21:10:47 -0200

    Finalizado Gracas a Deus!!!

--- src/questao2/Main.java
+++ src/questao2/Main.java
@@ -10,9 +10,22 @@ public class Main {
         v.setSalarioBruto(2000);
         v.setQuantidadeDeVendas(99);
         v.calcularSalarioLiquido();
-        
-        
+        System.out.println("Vendedor R$:");
         System.out.println(v.calcularSalarioLiquido());
+        
+        Estagiario e = new Estagiario();
+        e.setSalarioBruto(2500);
+        //e.setQuantidadeDeVendas(99);
+        e.calcularSalarioLiquido();
+        System.out.println("\nEstagiario R$:");
+        System.out.println(e.calcularSalarioLiquido());
+        
+        Funcionario f = new Funcionario();
+        f.setSalarioBruto(2500);
+        //e.setQuantidadeDeVendas(99);
+        f.calcularSalarioLiquido();
+        System.out.println("\nFuncionario R$:");
+        System.out.println(f.calcularSalarioLiquido());
         
     }   
 }
